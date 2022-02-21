ace9dac9c52b9a8ec8113fc641d0b7296318aa5a
Sikeru <wltn6513@gmail.com>
2022-02-21 17:26:06 +0900

    2022-02-21

---
+++ java/.metadata/.plugins/org.eclipse.core.resources/.history/a0/e02702f4e992001c1d03b25876b10101
@@ -0,0 +1,14 @@
+package sec01.ex01;
+
+import java.sql.SQLException;
+
+public class MemberDAO {
+	public List<MemberVO> listMembers(){
+		try {
+			
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		reurun membersList;
+	}
+}
--- java/pro17/src/sec01/ex01/MemberController.java
+++ java/pro17/src/sec01/ex01/MemberController.java
@@ -1,6 +1,7 @@
 package sec01.ex01;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletConfig;
@@ -10,7 +11,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/mem.do")
+@WebServlet("/member/*")
 public class MemberController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	MemberDAO memberDAO;
@@ -31,12 +32,31 @@ public class MemberController extends HttpServlet {
 	
 	protected void doHandle(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		String nextPage = null;
 		request.setCharacterEncoding("utf-8");
 		response.setContentType("text/html;charset=utf-8");
-		List<MemberVO> membersList = memberDAO.listMembers();
-		request.setAttribute("membersList", membersList);
-		RequestDispatcher dispatch = request.getRequestDispatcher("/test01/listMembers.jsp");
+		String action = request.getPathInfo();
+		System.out.println("action:" + action);
+		if (action == null || action.equals("/listMembers.do")) {
+			List<MemberVO> membersList = memberDAO.listMembers();
+			request.setAttribute("membersList", membersList);
+			nextPage = "/test02/listMembers.jsp";
+		} else if (action.equals("/addMember.do")) {
+			String id = request.getParameter("id");
+			String pwd = request.getParameter("pwd");
+			String name = request.getParameter("name");
+			String email = request.getParameter("email");
+			MemberVO memberVO = new MemberVO(id, pwd, name, email);
+			memberDAO.addMember(memberVO);
+			nextPage = "/member/listMembers.do";
+		} else if (action.equals("/memberForm.do")) {
+			nextPage = "/test02/memberForm.jsp";
+		} else {
+			List<MemberVO> membersList = memberDAO.listMembers();
+			request.setAttribute("membersList", membersList);
+			nextPage = "/test02/listMembers.jsp";
+		}
+		RequestDispatcher dispatch = request.getRequestDispatcher(nextPage);
 		dispatch.forward(request, response);
 	}
-
 }
