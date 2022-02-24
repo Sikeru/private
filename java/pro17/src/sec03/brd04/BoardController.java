ef7f023618390dc42731cb712ddf1900705226b5
Sikeru <wltn6513@gmail.com>
2022-02-24 17:06:12 +0900

    2022-02-24

--- java/pro17/src/sec03/brd04/BoardController.java
+++ java/pro17/src/sec03/brd04/BoardController.java
@@ -21,7 +21,7 @@ import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.apache.commons.io.FileUtils;
 
-@WebServlet("/board/*")
+/*@WebServlet("/board/*")*/
 public class BoardController extends HttpServlet {
 	private static String ARTICLE_IMAGE_REPO="C:\\board\\article_image";
 	BoardService boardService;
