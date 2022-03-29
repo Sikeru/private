b953b993676d2376a9158cedc827539b84805fec
Sikeru <wltn6513@gmail.com>
2022-03-29 14:43:26 +0900

    2022-03-29

---
+++ sts4/Chapter01/src/main/java/com/rubypaper/controller/BoardController.java
@@ -0,0 +1,18 @@
+package com.rubypaper.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BoardController {
+	
+	public BoardController(){
+		System.out.println("====> BoardController 생성");
+	}
+	
+	@GetMapping("/hello")
+	public String hello(String name) {
+		return "Hello : " + name;
+	}
+	
+}
